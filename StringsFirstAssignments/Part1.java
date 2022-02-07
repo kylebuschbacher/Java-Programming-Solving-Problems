f035880043fb727e3e8f9742ba6f4242cee3df7e
kylebuschbacher <96218986+kylebuschbacher@users.noreply.github.com>
2022-02-07 12:48:22 -0500

    Update Part1.java

--- StringsFirstAssignments/Part1.java
+++ StringsFirstAssignments/Part1.java
@@ -9,10 +9,22 @@ import java.io.File;
 public class Part1 {
     public String findSimpleGene (String dna) {
         //find the index position of the start codon "ATG"
+        int startCodon = dna.indexOf("ATG");
         //if no "ATG", return empty string
+        if (startCodon == -1) {
+            return "";
+        }
         //find the index position of the first stop codon "TAA"
+        int stopCodon = dna.indexOf("TAA", startCodon);
         //if no "TAA", return empty string
-        //check if distance between "ATG and "TAA" is multiple of 3
+        if (stopCodon == -1) {
+            return ""
+            }
+        //if distance between "ATG and "TAA" is multiple of 3
+        if ((stopCodon - startCodon) % 3 == 0) {
+            //return the string between startCodon and stopCodon
+        }
+        //else, return empty String:
         //need to update return statement
     return "";}
 
