66b94887f870cbf05e80047670c50f1464494baf
kylebuschbacher <96218986+kylebuschbacher@users.noreply.github.com>
2022-02-06 20:19:25 -0500

    Add files via upload

---
+++ StringsFirstAssignments/Part1.java
@@ -0,0 +1,19 @@
+import edu.duke.*;
+import java.io.File;
+/**
+ * Find a gene using the simplified algorithm
+ * 
+ * @author (Kyle) 
+ * @version (2/6/22)
+ */
+public class Part1 {
+    public String findSimpleGene (String dna) {
+        //find the index position of the start codon "ATG"
+        //if no "ATG", return empty string
+        //find the index position of the first stop codon "TAA"
+        //if no "TAA", return empty string
+        //check if distance between "ATG and "TAA" is multiple of 3
+        //need to update return statement
+    return "";}
+
+}
