f0efff7c929656da5d4dd5cb65ec3f4dbe3e8497
kylebuschbacher <96218986+kylebuschbacher@users.noreply.github.com>
2022-02-08 22:01:33 -0500

    Update Part1.java
    Works with two test cases. Need to put two more test cases, then Part 1 will be done.

--- StringsFirstAssignments/Part1.java
+++ StringsFirstAssignments/Part1.java
@@ -9,11 +9,39 @@ import java.io.File;
 public class Part1 {
     public String findSimpleGene (String dna) {
         //find the index position of the start codon "ATG"
+        int startCodon = dna.indexOf("ATG");
         //if no "ATG", return empty string
+        if (startCodon == -1) {
+            return "";
+        }
         //find the index position of the first stop codon "TAA"
+        int stopCodon = dna.indexOf("TAA", startCodon);
         //if no "TAA", return empty string
         //check if distance between "ATG and "TAA" is multiple of 3
+        if (stopCodon == -1) {
+            return "";
+            }
+        //if distance between "ATG and "TAA" is multiple of 3
+        if ((stopCodon - startCodon) % 3 == 0) {
+            //return the string between startCodon and stopCodon
+            return dna.substring(startCodon, stopCodon+3);
+        }
+        //else, return empty String:
         //need to update return statement
     return "";}
+    
+    public void testSimpleGene() {
+        //test case 1: DNA with ATG, TAA and the substring
+        //between them is a multiple of 3 (a gene)
+        String dna = "AAAAATGGAGTAATTTTTT";
+        System.out.println("DNA is " + dna);
+        String result = findSimpleGene(dna);
+        System.out.println("Gene is " + result);
+        //test case 2: DNA with no “ATG”
+        dna = "GAGTAA";
+        System.out.println("DNA is " + dna);
+        result = findSimpleGene(dna);
+        System.out.println("Gene is " + result);
+    }
 
 }
