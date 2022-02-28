0c3f85d35e01fcec0c6b9a1fb9e457a76f0a8ad5
kylebuschbacher <96218986+kylebuschbacher@users.noreply.github.com>
2022-02-27 19:45:59 -0500

    Add files via upload

--- StringsThirdAssignment/Part2.java
+++ StringsThirdAssignment/Part2.java
@@ -4,8 +4,10 @@
  * Finds the ratio of C's and G's to the length of an input string
    * 
  * @author Kyle 
- * @version 2/26/22
+ * @version 2/27/22
  */
+import edu.duke.*;
+
 public class Part2 {
     public double cgRatio(String dna){
         int count = 0;
@@ -60,4 +62,70 @@ public class Part2 {
         if (cgRatio(testDna) != 0.0/6) System.out.println("Case 8 Error");
         System.out.println("End of test");
     }
+    public int countCTG(String dna) {
+        int count = 0;
+        int startIndex = 0;
+        int currIndex = 0;
+        while (true) {
+            currIndex = dna.indexOf("CTG", startIndex);
+            if (currIndex == -1) break;
+            count ++;
+            startIndex = currIndex + 3;
+        }
+        return count;
+    }
+    public void testcountCTG() {
+        //case 1 1 CTG
+        String testdna = "xxxCTGyyy";
+        if (countCTG(testdna) == 1) System.out.println("Case 1 successful");
+        else System.out.println("Case 1 failure, count is " + countCTG(testdna));
+        //case 2 5 CTG
+        testdna = "xxxCTGyyyCTGsCTGCTGxxvCTGjjj";
+        if (countCTG(testdna) == 5) System.out.println("Case 2 successful");
+        else System.out.println("Case 2 failure, count is " + countCTG(testdna));
+        //case 3 0 CTG
+        testdna = "xxCCCCCCDFDFDFDTTTTTTGGGGGjjj";
+        if (countCTG(testdna) == 0) System.out.println("Case 3 successful");
+        else System.out.println("Case 3 failure, count is " + countCTG(testdna));
+        System.out.println("Test Completed");
+    }
+    public void processGenes(StorageResource sr) {
+        int plusNine = 0;
+        int plusCG = 0;
+        int longestLength = 0;
+        String longestString = "";
+        for (String s: sr.data()) {
+            if (s.length() > 60) {
+                System.out.println(s);
+                plusNine ++;                
+            }
+            if (cgRatio(s) > 0.35) {
+                System.out.println(s);
+                plusCG ++;
+            }
+            if (s.length() > longestLength){
+                longestLength = s.length();
+                longestString = s;
+            }
+        }
+        System.out.println("Number of strings longer than 9 characters: " + plusNine);
+        System.out.println("Number of strings with cg ratio > 0.35: " + plusCG);
+    }
+    public void testProcessGenes() {
+        StorageResource testSr= new StorageResource(); 
+        //testSr.add("xxx");
+        //testSr.add("xxxyyytttbbb");
+        //testSr.add("xxxCGCGxyxc");
+        //testSr.add("gggggggggggggggg");
+        //testSr.add("");
+        //processGenes(testSr);
+        FileResource fr = new FileResource("brca1line.fa");
+        String dna = fr.asString();
+        System.out.println("raw dna: " + dna);
+        Part1 testPart1 = new Part1();
+        testSr = testPart1.getAllGenes(dna);
+        testPart1.printAllGenes(dna);
+        processGenes(testSr);
+    }
+    
 }
