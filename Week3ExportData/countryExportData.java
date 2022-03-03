25966a2f6ee46edf5af39b60335192d31dc90f27
kylebuschbacher <96218986+kylebuschbacher@users.noreply.github.com>
2022-03-02 21:07:38 -0500

    Add files via upload

--- Week3ExportData/countryExportData.java
+++ Week3ExportData/countryExportData.java
@@ -12,11 +12,37 @@ public class countryExportData {
     public void tester(){
         FileResource fr = new FileResource();
         CSVParser parser = fr.getCSVParser();
+        System.out.println(countryInfo(parser, "Germany"));
+        
     }
     
     public String countryInfo(CSVParser parser, String country) {
+        //String output = "NOT FOUND"; // can I not do this? Strings immutable?
+        String tempOutput;
+        String output;
         for(CSVRecord record : parser) {
-            //do I use .get or .contains on record? read documentation.
+            String currCountry = record.get("Country");
+            //System.out.println(currCountry);
+            //System.out.println(country);
+            if (currCountry == country) {
+                tempOutput = currCountry;// + ": " + record.get("Exports")
+                //+ ": " + record.get("Value (dollars)");
+                break;
+            }
+                //This code is really messed up. I need to figure out
+                //how to tell when I've reached the end of this for loop
+                //read documentation
+                //also, can I not update strings after I created one
+                //
+                //also, here's an idea: just return the string here.
+                //if not found, just return "NOT FOUND".
         }
+        if (tempOutput != country) {
+            output = "NOT FOUND";
+        }
+        else {
+            output = tempOutput; 
+        }
+        return output;      
     }
 }
