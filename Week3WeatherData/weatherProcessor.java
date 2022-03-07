7dc9a98b35c4fa9d81aa43ae40235b9d4aeba803
kylebuschbacher <96218986+kylebuschbacher@users.noreply.github.com>
2022-03-07 07:30:16 -0500

    Add files via upload

--- Week3WeatherData/weatherProcessor.java
+++ Week3WeatherData/weatherProcessor.java
@@ -1,10 +1,24 @@
 
 /**
  * Write a description of weatherProcessor here.
+ * Find coldest hour in a csv file
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Kyle
+ * @version 3/7/22
  */
-public class weatherProcessor {
+import edu.duke.*;
+import org.apache.commons.csv.*;
 
+public class weatherProcessor {
+    public CSVRecord coldestHourInFile(CSVParser parser) {
+        CSVRecord coldestSoFar = null;
+        for (CSVRecord currentRow : parser){
+            //need to get currentRow's temperature, and convert to a number
+            double currentTemp = Double.parseDouble(currentRow.get("TemperatureF")
+            if (currentRow.get("TemperatureF") == -9999) continue;
+            if (currentRow == null) coldestSoFar = currentRow;
+            if (currentRow < coldestSoFar) coldestSoFar = currentRow;
+        }
+        return coldestSoFar;
+    }
 }
